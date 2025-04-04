deb0c3711d282a733cc6b80f48bc6556f2560843
Ezequiel_Weber <ezeweber2003@gmail.com>
2025-04-04 16:03:19 -0300

    Reordenamiento del codigo, al inicio el primer modulo que importo y ultimo el segundo, que a su vez llama dentro a mis otras funciones

--- src/funciones.py
+++ src/funciones.py
@@ -23,6 +23,14 @@ def mvp_puntos (ronda):
         ronda[mvp_ronda]["mvp"] += 1
     return ronda
 
+def imprimir_rondas(ranking, ronda_act, rondas):
+    print(f"Ranking ronda {ronda_act}:\n") if ronda_act < len(rondas) else print("Ranking final:\n")
+    print(f"{'Jugador':<12} {'Kills':>7} {'Asistencias':>12} {'Muertes':>9} {'MVPs':>6} {'Puntos':>8}")
+    print('-' * 60)
+    for nick, stats in ranking.items():
+        print(f"{nick:<12} {stats['kills']:>7} {stats['assists']:>12} {stats['deaths']:>9} {stats['mvp']:>6} {stats['puntos']:>8}")
+    print("-" * 60)
+
 def procesar_datos(rondas):
     ranking = {}
     for i, ronda in enumerate(rondas,start=1):
@@ -34,10 +42,6 @@ def procesar_datos(rondas):
             ranking[nick]["deaths"] += int(stats["deaths"])
             ranking[nick]["puntos"] += stats["puntos"]
             ranking[nick]["mvp"] += stats["mvp"]
-        # ranking_lista = list(ranking.items())
-        # print(ranking_lista)
-        # ranking_lista.sort(reverse=True, key=[stats]["puntos"])
-        # ranking = dict(ranking_lista)
         ranking = dict(sorted(ranking.items(), key=lambda item: item[1]["puntos"], reverse=True))
         imprimir_rondas(ranking, i,rondas)
     return ranking
@@ -46,11 +50,3 @@ def simulacion_rondas(rondas):
     for ronda in rondas:
         mvp_puntos(ronda) # calcula y almacena el puntaje en los campos nuevos para luego
     procesar_datos(rondas) # procesar los datos y hacer la simulacion de la partida
-
-def imprimir_rondas(ranking, ronda_act, rondas):
-    print(f"Ranking ronda {ronda_act}:\n") if ronda_act < len(rondas) else print("Ranking final:\n")
-    print(f"{'Jugador':<12} {'Kills':>7} {'Asistencias':>12} {'Muertes':>9} {'MVPs':>6} {'Puntos':>8}")
-    print('-' * 60)
-    for nick, stats in ranking.items():
-        print(f"{nick:<12} {stats['kills']:>7} {stats['assists']:>12} {stats['deaths']:>9} {stats['mvp']:>6} {stats['puntos']:>8}")
-    print("-" * 60)
